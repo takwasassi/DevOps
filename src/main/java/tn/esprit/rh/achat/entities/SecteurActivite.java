1f45d4331dfc5fd66aaf71d2f607c173d08d3ee8
takwasassi <takwa.sassi@esprit.tn>
2022-11-10 12:53:46 +0100

    commit

---
+++ src/main/java/tn/esprit/rh/achat/entities/SecteurActivite.java
@@ -0,0 +1,60 @@
+package tn.esprit.rh.achat.entities;
+
+import java.io.Serializable;
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SecteurActivite implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idSecteurActivite;
+	private String codeSecteurActivite;
+	private String libelleSecteurActivite;
+	@ManyToMany(mappedBy="secteurActivites")
+	@JsonIgnore
+	private Set<Fournisseur> fournisseurs;
+	public Long getIdSecteurActivite() {
+		return idSecteurActivite;
+	}
+	public void setIdSecteurActivite(Long idSecteurActivite) {
+		this.idSecteurActivite = idSecteurActivite;
+	}
+	public String getCodeSecteurActivite() {
+		return codeSecteurActivite;
+	}
+	public void setCodeSecteurActivite(String codeSecteurActivite) {
+		this.codeSecteurActivite = codeSecteurActivite;
+	}
+	public String getLibelleSecteurActivite() {
+		return libelleSecteurActivite;
+	}
+	public void setLibelleSecteurActivite(String libelleSecteurActivite) {
+		this.libelleSecteurActivite = libelleSecteurActivite;
+	}
+	public Set<Fournisseur> getFournisseurs() {
+		return fournisseurs;
+	}
+	public void setFournisseurs(Set<Fournisseur> fournisseurs) {
+		this.fournisseurs = fournisseurs;
+	}
+	
+	
+}
